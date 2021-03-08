fbcb4113b13a32607449bc247b2e7227f62f5100
Brian Fernando <brianfdo@users.noreply.github.com>
2021-03-07 19:10:26 -0500

    make function compatible for all stock tickers

---
+++ lrm.py
@@ -0,0 +1,39 @@
+import quandl
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import datetime
+
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn import preprocessing
+
+def predictFutureStockPrices(ticker, days):
+    quandl.ApiConfig.api_key = 'ekUM3Ut18s6GyHRFqxV9'
+
+    df = quandl.get("WIKI/" + ticker.upper(), paginate = False)
+    df = df[['Adj. Close']]
+    pred_days = days
+    df['Prediction'] = df['Adj. Close'].shift(-pred_days)
+
+    X = np.array(df.drop(['Prediction'], 1))
+    X = preprocessing.scale(X)
+    X_pred = X[-pred_days:]
+    X = X[:-pred_days]
+
+    Y = np.array(df['Prediction'])
+    Y = Y[:-pred_days]  
+
+    try:
+        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.2)
+    except:
+        print(X.shape)
+        print(Y.shape)
+
+    clf = LinearRegression()
+    clf.fit(X_train, Y_train)
+    confidence = clf.score(X_test, Y_test)
+
+    forecast = clf.predict(X_pred)
+    
+
